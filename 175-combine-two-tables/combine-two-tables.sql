66f4484a4759cb3793ff57fbc66a4ebc9f1bed8b
Rakesh Pai <41351936+developer-rpai@users.noreply.github.com>
2025-07-18 15:19:44 -0500

    Time: 362 ms (94.55%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,5 +1,13 @@
 # Write your MySQL query statement below
-select p.firstName, p.lastName, a.city, a.state
-from Person p
-left join Address a
-on p.personId = a.personID
+-- Write a solution to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
+-- SELECT columns listed
+-- LEFT JOIN with Address Table
+
+SELECT 
+    p.firstName,
+    p.lastName,
+    a.city,
+    a.state
+FROM Person p
+LEFT JOIN Address a
+ON p.personId = a.personId
